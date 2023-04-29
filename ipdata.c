7a0afe6cb2a749bf0a85b2bfeb3649d8636f761c
bashiecrashie <dmitriydanilovdanilov2016@gmail.com>
2023-04-29 23:38:19 +0300

    Файл исходного кода структур для хранения адресов и масок

---
+++ ipdata.c
@@ -0,0 +1,27 @@
+typedef struct BinData
+{
+    //Переменная для хранения размера двоичной октеты
+    int BinaryOctetSize;
+    //Массивы для хранения IP в двоичной системе счисления
+    int BinaryIPFirstOctet[8], BinaryIPSecondOctet[8], BinaryIPThirdOctet[8], BinaryIPFourthOctet[8];    
+    //Массивы для хранения маски в двоичной системе счисления
+    int BinaryMaskFirstOctet[8], BinaryMaskSecondOctet[8], BinaryMaskThirdOctet[8], BinaryMaskFourthOctet[8];
+    //Массивы для хранения содержимого октет сетевого адреса в двоичном виде
+    int BinaryNetworkAddressFirstOctet[8], BinaryNetworkAddressSecondOctet[8], BinaryNetworkAddressThirdOctet[8], BinaryNetworkAddressFourthOctet[8];
+}BinData;
+
+//Массив для хранения сетевого адреса в десятичном виде
+static int NetworkDecimalIP[4];
+typedef struct DecData
+{
+    //Переменные для хранения размера октет и их количества
+    int DecimalOctetSize;
+    int OctetNumber;
+    int DecimalIP[4];
+    int DecimalMask[4];
+    //Массивы с для адресов
+    int Network_1[4];
+    int Network_2[4];
+    int Network_3[4];
+    int Network_4[4];
+}DecData;
