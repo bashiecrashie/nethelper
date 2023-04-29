89c883c3dd6f626992b3059e2fc05d88fe104813
bashiecrashie <dmitriydanilovdanilov2016@gmail.com>
2023-04-29 23:43:11 +0300

    Масивы для хранения адресов и масок перенесены в структуры

--- main.c
+++ main.c
@@ -3,181 +3,149 @@
 #include<stdlib.h>
 #include"calcfuncs.h"//файл с объявлением функций для расчётов
 #include"ipfuncs.h"//файл с объявлением функций для определения типа ip, получения адреса сети и т.п.
+#include"ipdata.h"
 
 int main(int argc, char *argv[])
 {
-
     //Для красоты :-)
     char *logo = "\n"                                                      
 " _____ _____ _____ _____ _____ __    _____ _____ _____ \n"
 "|   | |   __|_   _|  |  |   __|  |  |  _  |   __| __  |\n"
 "| | | |   __| | | |     |   __|  |__|   __|   __|    -|\n"
 "|_|___|_____| |_| |__|__|_____|_____|__|  |_____|__|__|\n"
-"                                            Version 0.2\n";
+"                                            Version 0.3\n";
 
     printf("%s", logo);                                                   
-
-    //Размеры для массивов с данными
-    const int BinaryOctetSize = 8, DecimalOctetSize = 3, OctetNumber = 4, MaxPrefixSize = 32;
-
-    //Числовые массивы для IP в двоичном представлении
-    int BinaryIPFirstOctet[BinaryOctetSize], BinaryIPSecondOctet[BinaryOctetSize], BinaryIPThirdOctet[BinaryOctetSize], BinaryIPFourthOctet[BinaryOctetSize];
-
-    //Числовые массивы для маски в двоичном представлении
-    int BinaryMaskFirstOctet[BinaryOctetSize], BinaryMaskSecondOctet[BinaryOctetSize], BinaryMaskThirdOctet[BinaryOctetSize], BinaryMaskFourthOctet[BinaryOctetSize], BinaryPrefix[MaxPrefixSize];
-
-    //Числовык массивы для IP и маски в десятичном представлении
-    int NetworkDecimalIP[OctetNumber], DecimalIP[OctetNumber], DecimalMask[OctetNumber];
-
+    //Переменная структуры с двоиным представлением адресов и масок
+    BinData bin;
+    //Переменная структуры с десятичным представлением адресов и масок
+    DecData dec;
+    //Присваиваем полям структуры значения
+    bin.BinaryOctetSize = 8;
+    dec.OctetNumber = 4;
     //Переменная для сетевого префикса
     int prefix = 0;
-
     //Переменные-счётчики
     int counter = 0, PrefixCheck = 0;
-
     //Переменные-указатели
     int *OutPointer;
-
     //Проверка количества аргументов
     if(argc != 2){ printf("Неверный формат. Пример: 123.123.123.123/12\n"); return 1; }
-
     //Занесение данных в массив и переменную
-    sscanf(argv[1], "%d.%d.%d.%d/%d", &DecimalIP[0], &DecimalIP[1], &DecimalIP[2], &DecimalIP[3], &prefix);
-
+    sscanf(argv[1], "%d.%d.%d.%d/%d", &dec.DecimalIP[0], &dec.DecimalIP[1], &dec.DecimalIP[2], &dec.DecimalIP[3], &prefix);
     //Проверка правильности адреса
     for(counter = 0; counter < 3; ++counter)
-
-        if(DecimalIP[counter] > 254){ printf("Октета в адресе не может быть больше, чем 254.\n"); break; return 1; }
-
+        if(dec.DecimalIP[counter] > 254){ printf("Октета в адресе не может быть больше, чем 254.\n"); break; return 1; }
     //Проверка правильности префикса сети
     if(prefix > 32){ printf("Префикс сети не может быть больше, чем 32\n"); return 1; }
-
     //Вывод данных
-    printf("IP адрес в десятичном виде: %d.%d.%d.%d\n", DecimalIP[0], DecimalIP[1], DecimalIP[2], DecimalIP[3]);
+    printf("IP адрес в десятичном виде: %d.%d.%d.%d\n", dec.DecimalIP[0], dec.DecimalIP[1], dec.DecimalIP[2], dec.DecimalIP[3]);
     printf("Префикс сети в десятичном виде: %d\n", prefix);
-
     //Заполняем первую октету IP
-    OutPointer = dectobin(DecimalIP[0]);
-    for(counter = 0; counter < BinaryOctetSize; ++counter)
-        BinaryIPFirstOctet[counter] = OutPointer[counter];
-
+    OutPointer = dectobin(dec.DecimalIP[0]);
+    for(counter = 0; counter < bin.BinaryOctetSize; ++counter)
+        bin.BinaryIPFirstOctet[counter] = OutPointer[counter];
     //Заполняем вторую октету IP
-    OutPointer = dectobin(DecimalIP[1]);
-    for(counter = 0; counter < BinaryOctetSize; ++counter)
-        BinaryIPSecondOctet[counter] = OutPointer[counter];
-
+    OutPointer = dectobin(dec.DecimalIP[1]);
+    for(counter = 0; counter < bin.BinaryOctetSize; ++counter)
+        bin.BinaryIPSecondOctet[counter] = OutPointer[counter];
     //Заполняем третью IP
-    OutPointer = dectobin(DecimalIP[2]);
-    for(counter = 0; counter < BinaryOctetSize; ++counter)
-        BinaryIPThirdOctet[counter] = OutPointer[counter];
-
+    OutPointer = dectobin(dec.DecimalIP[2]);
+    for(counter = 0; counter < bin.BinaryOctetSize; ++counter)
+        bin.BinaryIPThirdOctet[counter] = OutPointer[counter];
     //Заполняем четвёртую октету IP
-    OutPointer = dectobin(DecimalIP[3]);
-    for(counter = 0; counter < BinaryOctetSize; ++counter)
-        BinaryIPFourthOctet[counter] = OutPointer[counter];
-
+    OutPointer = dectobin(dec.DecimalIP[3]);
+    for(counter = 0; counter < bin.BinaryOctetSize; ++counter)
+        bin.BinaryIPFourthOctet[counter] = OutPointer[counter];
     //Выводим содержание октет
     printf("IP в двоичном представлении: ");
-    for(counter = 0; counter < BinaryOctetSize; ++counter)
-        printf("%d", BinaryIPFirstOctet[counter]);
+    for(counter = 0; counter < bin.BinaryOctetSize; ++counter)
+        printf("%d", bin.BinaryIPFirstOctet[counter]);
     printf(".");
-    for(counter = 0; counter < BinaryOctetSize; ++counter)
-        printf("%d", BinaryIPSecondOctet[counter]);
+    for(counter = 0; counter < bin.BinaryOctetSize; ++counter)
+        printf("%d", bin.BinaryIPSecondOctet[counter]);
     printf(".");
-    for(counter = 0; counter < BinaryOctetSize; ++counter)
-        printf("%d", BinaryIPThirdOctet[counter]);
+    for(counter = 0; counter < bin.BinaryOctetSize; ++counter)
+        printf("%d", bin.BinaryIPThirdOctet[counter]);
     printf(".");
-    for(counter = 0; counter < BinaryOctetSize; ++counter)
-        printf("%d", BinaryIPFourthOctet[counter]);
+    for(counter = 0; counter < bin.BinaryOctetSize; ++counter)
+        printf("%d", bin.BinaryIPFourthOctet[counter]);
     printf("\n");
-
     //Инициализируем нулями массивы для маски
-    for(counter = 0; counter < BinaryOctetSize; ++counter)
-        BinaryMaskFirstOctet[counter] = 0; BinaryMaskSecondOctet[counter] = 0; BinaryMaskThirdOctet[counter] = 0; BinaryMaskFourthOctet[counter] = 0;
-
+    for(counter = 0; counter < bin.BinaryOctetSize; ++counter)
+        bin.BinaryMaskFirstOctet[counter] = 0; bin.BinaryMaskSecondOctet[counter] = 0; bin.BinaryMaskThirdOctet[counter] = 0; bin.BinaryMaskFourthOctet[counter] = 0;
     //Заполняем первую октету для маски в двоичном виде
-    for(counter = 0; counter < BinaryOctetSize; ++counter)
+    for(counter = 0; counter < bin.BinaryOctetSize; ++counter)
     {
         if(PrefixCheck < prefix)
         {
-            BinaryMaskFirstOctet[counter] = 1;
+            bin.BinaryMaskFirstOctet[counter] = 1;
             ++PrefixCheck;
         }else if(PrefixCheck == prefix)
-            BinaryMaskFirstOctet[counter] = 0;
+            bin.BinaryMaskFirstOctet[counter] = 0;
     }
     //Заполняем вторую октету для маски в двоичном виде
-    for(counter = 0; counter < BinaryOctetSize; ++counter)
+    for(counter = 0; counter < bin.BinaryOctetSize; ++counter)
     {
         if(PrefixCheck < prefix)
         {
-            BinaryMaskSecondOctet[counter] = 1;
+            bin.BinaryMaskSecondOctet[counter] = 1;
             ++PrefixCheck;
         }else if(PrefixCheck == prefix)
-            BinaryMaskSecondOctet[counter] = 0;
+            bin.BinaryMaskSecondOctet[counter] = 0;
     }
     //Заполняем третью октету для маски в двоичном виде
-    for(counter = 0; counter < BinaryOctetSize; ++counter)
+    for(counter = 0; counter < bin.BinaryOctetSize; ++counter)
     {
         if(PrefixCheck < prefix)
         {
-            BinaryMaskThirdOctet[counter] = 1;
+            bin.BinaryMaskThirdOctet[counter] = 1;
             ++PrefixCheck;
         }else if(PrefixCheck == prefix)
-            BinaryMaskThirdOctet[counter] = 0;
+            bin.BinaryMaskThirdOctet[counter] = 0;
     }
     //Заполняем четвёртую октету для маски в двоичном виде
-    for(counter = 0; counter < BinaryOctetSize; ++counter)
+    for(counter = 0; counter < bin.BinaryOctetSize; ++counter)
     {
         if(PrefixCheck < prefix)
         {
-            BinaryMaskFourthOctet[counter] = 1;
+            bin.BinaryMaskFourthOctet[counter] = 1;
             ++PrefixCheck;
         }else if(PrefixCheck == prefix)
-            BinaryMaskFourthOctet[counter] = 0;
+            bin.BinaryMaskFourthOctet[counter] = 0;
     }
-
     //Выводим маску в двоичном виде
     printf("Префикс сети в двоичном представлении:");
-    for(counter = 0; counter < BinaryOctetSize; ++counter)
-        printf("%d", BinaryMaskFirstOctet[counter]);
+    for(counter = 0; counter < bin.BinaryOctetSize; ++counter)
+        printf("%d", bin.BinaryMaskFirstOctet[counter]);
     printf(".");
-    for(counter = 0; counter < BinaryOctetSize; ++counter)
-        printf("%d", BinaryMaskSecondOctet[counter]);
+    for(counter = 0; counter < bin.BinaryOctetSize; ++counter)
+        printf("%d", bin.BinaryMaskSecondOctet[counter]);
     printf(".");
-    for(counter = 0; counter < BinaryOctetSize; ++counter)
-        printf("%d", BinaryMaskThirdOctet[counter]);
+    for(counter = 0; counter < bin.BinaryOctetSize; ++counter)
+        printf("%d", bin.BinaryMaskThirdOctet[counter]);
     printf(".");
-    for(counter = 0; counter < BinaryOctetSize; ++counter)
-        printf("%d", BinaryMaskFourthOctet[counter]);
+    for(counter = 0; counter < bin.BinaryOctetSize; ++counter)
+        printf("%d", bin.BinaryMaskFourthOctet[counter]);
     printf("\n");
-
     //Заполняем массив преобразованной в десятичный вид маски
-    DecimalMask[0] = bintodec(BinaryMaskFirstOctet);
-    DecimalMask[1] = bintodec(BinaryMaskSecondOctet);
-    DecimalMask[2] = bintodec(BinaryMaskThirdOctet);
-    DecimalMask[3] = bintodec(BinaryMaskFourthOctet);
-
+    dec.DecimalMask[0] = bintodec(bin.BinaryMaskFirstOctet);
+    dec.DecimalMask[1] = bintodec(bin.BinaryMaskSecondOctet);
+    dec.DecimalMask[2] = bintodec(bin.BinaryMaskThirdOctet);
+    dec.DecimalMask[3] = bintodec(bin.BinaryMaskFourthOctet);
     //Выводим маску в десятичном виде
-    printf("Маска сети: %d.%d.%d.%d\n", DecimalMask[0], DecimalMask[1], DecimalMask[2], DecimalMask[3]);
-
+    printf("Маска сети: %d.%d.%d.%d\n", dec.DecimalMask[0], dec.DecimalMask[1], dec.DecimalMask[2], dec.DecimalMask[3]);
     //Определяем тип IP
-    iptype(DecimalIP[0]);
-
+    iptype(dec.DecimalIP[0]);
     //Формируем адрес сети
-    OutPointer = networkaddress(BinaryIPFirstOctet, BinaryIPSecondOctet, BinaryIPThirdOctet, BinaryIPFourthOctet, BinaryMaskFirstOctet, BinaryMaskSecondOctet, BinaryMaskThirdOctet, BinaryMaskFourthOctet);
-
+    OutPointer = networkaddress(bin.BinaryIPFirstOctet, bin.BinaryIPSecondOctet, bin.BinaryIPThirdOctet, bin.BinaryIPFourthOctet, bin.BinaryMaskFirstOctet, bin.BinaryMaskSecondOctet, bin.BinaryMaskThirdOctet, bin.BinaryMaskFourthOctet);
     //Заполняем массив адресом сети
-    for(counter = 0; counter < OctetNumber; ++counter)
+    for(counter = 0; counter < dec.OctetNumber; ++counter)
         NetworkDecimalIP[counter] = OutPointer[counter];
-
     //Выводим адрес сети
     printf("Адрес сети в десятичном представлении: %d.%d.%d.%d\n", NetworkDecimalIP[0], NetworkDecimalIP[1], NetworkDecimalIP[2], NetworkDecimalIP[3]);
-
     //Проверка на предмет широковещательного адреса
-    broadcastaddr(DecimalIP[3]);
-
+    broadcastaddr(dec.DecimalIP[3]);
     //Проверка на принадлежность к одной из сетей
     networkbelong(NetworkDecimalIP);
-
-
 }
